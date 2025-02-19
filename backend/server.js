7951635951560ebd814761fb6a1cae94a025e9fe
Christian <Signal04@gmx.de>
2025-02-19 16:16:37 +0100

    fertige webapp

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const cors =require("cors");
+const cors = require("cors");
 const mysql = require("mysql");
-const app = express();
 
+const app = express();
+app.use(express.json());
 app.use(cors());
 
 const db = mysql.createConnection({
@@ -17,6 +18,44 @@ app.get("/", (req, res) => {
     const sql = "SELECT * FROM student";
     db.query(sql, (err, data) => {
         if (err) return res.json("Error");
+        return res.json(data);
+    });
+});
+
+app.post('/create', (req, res) => {
+    const sql = "INSERT INTO student (`Name`, `Email`) VALUES (?)";
+    const values = [
+        req.body.name,
+        req.body.email
+    ];
+
+    db.query(sql, [values], (err, data) => {
+        if (err) return res.json("Error");
+        return res.json(data);
+    });
+});
+
+app.put('/update/:id', (req, res) => {
+    const sql = "UPDATE student SET Name=?, Email=? WHERE id=?";
+    const values = [
+        req.body.name,
+        req.body.email
+    ];
+    const id = req.params.id;
+
+    db.query(sql, [...values, id], (err, data) => {
+        if (err) return res.json("Error");
+        return res.json(data);
+    });
+});
+
+app.delete('/student/:id', (req, res) => {
+    const sql = "DELETE FROM student WHERE id=?";
+    const id = req.params.id;
+
+    db.query(sql, [id], (err, data) => {
+        if (err) return res.json("Error");
+        if (data.affectedRows === 0) return res.json("No record found to delete");
         return res.json(data);
     });
 });
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import CreateStudent from './CreateStudent.js'
 import Student from './Student.js'
+import UpdateStudent from './UpdateStudent.js';
+
 
 function App() {
   return (
@@ -9,6 +12,8 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path='/' element={<Student />}></Route>
+        <Route path='/create' element={<CreateStudent />}></Route>
+        <Route path='/update/:id' element={<UpdateStudent />}></Route>
       </Routes>
       </BrowserRouter>
     </div>
