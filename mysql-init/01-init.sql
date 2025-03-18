82557aabc26a7c223afc9069fc81050dcc636118
niritokun03@gmail.com <niritokun03@gmail.com>
2025-03-18 17:07:18 +0100

    Funktionierende-Noten-Struktur

--- mysql-init/01-init.sql
+++ mysql-init/01-init.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS note (
+CREATE TABLE IF NOT EXISTS Note (
   ID INT AUTO_INCREMENT PRIMARY KEY,
   Semester INT,
   Modulname VARCHAR(255),
@@ -8,6 +8,6 @@ CREATE TABLE IF NOT EXISTS note (
   Note2 INT
 );
 
-INSERT INTO note (Semester, Modulname, Leistungspunkte, Note0, Note1, Note2) VALUES
-(1, 'Wirtschaftsmathematik', 5, 2, 0, 0),
-(1, 'Materialwirtschaft', 4, 1, 0, 0);
+INSERT INTO Note (Semester, Modulname, Leistungspunkte, Note0, Note1, Note2) VALUES
+(1, 'Wirtschaftsmathematik', 5, 2.4, 0, 0),
+(1, 'Materialwirtschaft', 4, 1.8, 0, 0);
